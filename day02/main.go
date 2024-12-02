8c631871015a37618510eb0712d6d505cbf1d2f7
Christian Maulany <cmaulany@gmail.com>
2024-12-02 16:40:59 +0700

    day 2

---
+++ day02/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type input = [][]int
+
+func main() {
+	input := load()
+
+	answerPart1 := part1(input)
+	fmt.Printf("Answer part 1: %d\n", answerPart1)
+
+	answerPart2 := part2(input)
+	fmt.Printf("Answer part 2: %d\n", answerPart2)
+}
+
+func load() input {
+	file, _ := os.Open("inputs/day02.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	r, _ := regexp.Compile("\\d+")
+	var input [][]int
+	for scanner.Scan() {
+		line := scanner.Text()
+		match := r.FindAllString(line, 8)
+		var l []int
+		for _, s := range match {
+			n, _ := strconv.Atoi(s)
+			l = append(l, n)
+		}
+		input = append(input, l)
+	}
+	return input
+}
+
+func part1(input input) int {
+	sum := 0
+	for _, line := range input {
+		if isSafe((line)) {
+			sum++
+		}
+	}
+	return sum
+}
+
+func part2(input input) int {
+	sum := 0
+	for _, line := range input {
+	loop:
+		for i := 0; i < len(line); i++ {
+			lineCopy := make([]int, len(line))
+			copy(lineCopy, line)
+			modifiedLine := append(lineCopy[:i], lineCopy[i+1:]...)
+			if isSafe(modifiedLine) {
+				sum++
+				break loop
+			}
+		}
+	}
+	return sum
+}
+
+func isSafe(level []int) bool {
+	dir := 1
+	if level[1] < level[0] {
+		dir = -1
+	}
+	for i := 0; i < len(level)-1; i++ {
+		a := level[i]
+		b := level[i+1]
+		if (b-a)*dir < 1 {
+			return false
+		}
+		delta := b - a
+		if delta < 0 {
+			delta *= -1
+		}
+		if delta < 1 || delta > 3 {
+			return false
+		}
+	}
+	return true
+}
